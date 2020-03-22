e9c71943ce54384927f781befe2aaa677626243e
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2020-03-22 11:21:35 +0100

    backup master branch

--- PrimeNumber/src/Algorithms/Algorithm.cpp
+++ PrimeNumber/src/Algorithms/Algorithm.cpp
@@ -3,7 +3,6 @@
 void Algorithm::CheckResult(mpz_t z_n, mpz_t z_p, mpz_t z_q){
 	mpz_t p_mul_q;
 	mpz_init(p_mul_q);
-
 	mpz_mul(p_mul_q, z_p, z_q);
 	if(mpz_cmp(z_n, p_mul_q) == 0){
 		gmp_printf("OK => %Zd = %Zd * %Zd\n", z_n, z_p, z_q);
