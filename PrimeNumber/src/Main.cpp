91bae6c81f20ddda6c01e52b80456b007aa44346
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2021-01-28 19:37:10 +0100

    refactor

--- PrimeNumber/src/Main.cpp
+++ PrimeNumber/src/Main.cpp
@@ -4,8 +4,8 @@
 //BENCHMARK_MAIN();
 
 int main(int argc, char *argv[]){
-	AlgorithmsAbstracts::IFactorization *factorization = new AlgorithmsFactorization::TrialDivision();
-	factorization->Factor(Elements::MyHelper::GetSemiPrime(10));
+	AlgorithmsAbstracts::IFactorization *factorization = new AlgorithmsFactorization::QuadraticSieve();
+	factorization->Factor(Elements::MyHelper::GetSemiPrime(20));
 	factorization->CheckResult(true);
 	delete factorization;
 	return 0;
